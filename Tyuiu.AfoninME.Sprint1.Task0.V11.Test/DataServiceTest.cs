37200bfce437544a8f7f0ffb6a313c2816b6a086
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-10-01 20:36:43 +0500

    Add project files.

---
+++ Tyuiu.AfoninME.Sprint1.Task0.V11.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.AfoninME.Sprint1.Task0.V11.Lib;
+//Задание
+//Написать программуб которая вычисляет выражение 4*5/2-18/2/3 и печатает результат на экране
+
+namespace Tyuiu.AfoninME.Sprint1.Task0.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+
+
+
+        }
+    }
+}
---
+++ Tyuiu.AfoninME.Sprint1.Task0.V11/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Tyuiu.AfoninME.Sprint1.Task0.V11.Lib;
+
+
+
+namespace Tyuiu.AfoninME.Sprint1.Task0.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Афонин М. Е. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Афонин Максим Евгеньевич | ИСТНб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 4*5/2-18/2/3            *");
+            Console.WriteLine("* и печатает результут на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 4*5/2-18/2/3                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+
+        }
+    }
+}
+
+
