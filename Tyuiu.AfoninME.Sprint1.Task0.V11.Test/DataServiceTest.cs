55fb36b5cf2f70f30a4657f0d0b88e5b6777b8fd
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-10-02 23:29:06 +0500

    Task1

--- Tyuiu.AfoninME.Sprint1.Task0.V11.Test/DataServiceTest.cs
+++ Tyuiu.AfoninME.Sprint1.Task0.V11.Test/DataServiceTest.cs
@@ -12,7 +12,7 @@ namespace Tyuiu.AfoninME.Sprint1.Task0.V11.Test
         {
             DataService ds = new DataService();
             var res = ds.Calculate();
-            Assert.AreEqual(2, res);
+            Assert.AreEqual(6, res);
 
 
 
---
+++ Tyuiu.AfoninME.Sprint1.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+    
+
+namespace Tyuiu.AfoninME.Sprint1.Task1.V4.Lib
+{
+    public class DataService : ISprint1Task1V4
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x + y) / x * y;
+        }
+    }
+}
