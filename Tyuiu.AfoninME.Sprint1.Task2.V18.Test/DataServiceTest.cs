721e0e16d56f5db45dabbc0a261c199b8fbee258
Maksim Afonin <Tyuiu.AfoninME@outlook.com>
2025-10-03 18:53:27 +0500

    Task2

---
+++ Tyuiu.AfoninME.Sprint1.Task2.V18.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.AfoninME.Sprint1.Task2.V18.Lib;
+namespace Tyuiu.AfoninME.Sprint1.Task2.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            
+            int expected = 40;
+            int result = ds.CalculateSideSquareParallelepiped(2, 3, 4);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Tyuiu.AfoninME.Sprint1.Task2.V18/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AfoninME.Sprint1.Task2.V18.Lib;
+namespace Tyuiu.AfoninME.Sprint1.Task2.V18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int value, valueTwo, valueThree;
+            Console.WriteLine("Введите длину параллелепипеда:");
+            value = (int)Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Введите ширину параллелепипеда:");
+            valueTwo = (int)Convert.ToUInt32(Console.ReadLine());
+            Console.WriteLine("Введите высоту параллелепипеда:");
+            valueThree = (int)Convert.ToUInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int z = ds.CalculateSideSquareParallelepiped(value, valueTwo, valueThree);
+
+            Console.WriteLine($"Площадь боковой поверхности параллелепипеда: {z}");
+
+            Console.ReadLine();
+
+        }
+    }
+}
